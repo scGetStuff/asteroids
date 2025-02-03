81c9c98e5c88b2d15ab6caaf13ed4a69651b77ad
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-02-03 16:19:28 -0500

    add movement to player

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ def main():
     numpass, numfail = pygame.init()
     print(f"init: {pygame.get_init()}  numpass: {numpass}  numfail: {numfail}")
 
+    game()
+
+
+def game():
     screen = pygame.display.set_mode((CONST.SCREEN_WIDTH, CONST.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(CONST.SCREEN_WIDTH / 2, CONST.SCREEN_HEIGHT / 2)
 
     while True:
@@ -21,7 +30,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for thingy in drawable:
+            thingy.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -16,8 +16,25 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt):
+        self.rotation += CONST.PLAYER_TURN_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
-        pass
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rotate(-dt)
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * CONST.PLAYER_SPEED * dt
