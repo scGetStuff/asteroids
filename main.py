311295512c387168f385946971353f62673f399c
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-02-03 14:37:24 -0500

    add black empty window

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ def main():
     print(f"Screen width: {CONST.SCREEN_WIDTH}")
     print(f"Screen height: {CONST.SCREEN_HEIGHT}")
 
+    numpass, numfail = pygame.init()
+    print(f"init: {pygame.get_init()}  numpass: {numpass}  numfail: {numfail}")
+
+    screen = pygame.display.set_mode((CONST.SCREEN_WIDTH, CONST.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(0)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
