ba5ed0fb4d3503bead97ce1ab8194c2ef005ed58
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-02-03 14:19:23 -0500

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# asteroids
-boot.dev "Asteroids" project
+# "Asteroids" project
+
+-   https://www.boot.dev/
+-   https://github.com/scGetStuff/bookbot
---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import constants as CONST
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {CONST.SCREEN_WIDTH}")
+    print(f"Screen height: {CONST.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
