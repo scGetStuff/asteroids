d26187435b98b1132b2b14eed09a74ad70b26009
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-02-03 16:54:14 -0500

    add round space rocks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import constants as CONST
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -21,9 +23,13 @@ def game():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     player = Player(CONST.SCREEN_WIDTH / 2, CONST.SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
